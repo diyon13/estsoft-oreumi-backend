c5fec949623c7c20687b8edbab6463adb99fb3ff
Kim, Jaeung <kimjw5076@naver.com>
2025-11-28 16:27:58 +0900

    day01(1128)-Java 연습문제
    day10

---
+++ 14-java/src/day01/day01Ex01.java
@@ -0,0 +1,16 @@
+package day01;
+
+public class day01Ex01 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+
+        System.out.printf("x: %d, y: %d\n", x, y);
+        System.out.printf("x + y = %d\n", x + y);
+        System.out.printf("x - y = %d\n", x - y);
+        System.out.printf("x * y = %d\n", x * y);
+        System.out.printf("x / y = %.2f\n", (double)x / y);
+//        System.out.printf("x %% y = %d\n", x % y);
+        System.out.println("x % y = " + x % y);
+    }
+}
