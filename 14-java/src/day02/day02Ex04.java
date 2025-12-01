758719204dbf29b18bb10e7b0ad074d366a5bfd3
Kim, Jaeung <kimjw5076@naver.com>
2025-12-02 07:55:33 +0900

    day02(1201) - 숙제

---
+++ 14-java/src/day02/day02Ex04.java
@@ -0,0 +1,29 @@
+package day02;
+
+import java.util.Scanner;
+
+public class day02Ex04 {
+    public static void main(String[] args) {
+        int num;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter a positive integer greater than 1: ");
+        num = in.nextInt();
+        System.out.println();
+
+        in.close();
+
+        int a = 0;
+        int b = 1;
+        int c = 0;
+
+        for (int i = 2; i <= num; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+
+        System.out.printf("%dth Fibonacci number = %d\n", num, c);
+    }
+}
