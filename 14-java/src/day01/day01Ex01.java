31e6376c8d571349fca243280532b4ca38d7233f
Kim, Jaeung <kimjw5076@naver.com>
2025-12-03 16:17:10 +0900

    Update day01Ex01.java

--- 14-java/src/day01/day01Ex01.java
+++ 14-java/src/day01/day01Ex01.java
@@ -4,7 +4,6 @@ public class day01Ex01 {
     public static void main(String[] args) {
         int x = 10;
         int y = 3;
-
         System.out.printf("x: %d, y: %d\n", x, y);
         System.out.printf("x + y = %d\n", x + y);
         System.out.printf("x - y = %d\n", x - y);
