66dd3c6418b196b8b14c4053118e472cc09d0255
Kim, Jaeung <kimjw5076@naver.com>
2025-12-10 17:14:53 +0900

    day07(1208) 수정

--- 14-java/src/day07/Ex02.java
+++ 14-java/src/day07/Ex02.java
@@ -4,6 +4,8 @@ import java.util.InputMismatchException;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 public class Ex02 {
     public static void main(String[] args) {
         int korean;
