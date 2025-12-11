8a69ba14bee85c056dc61898dff91b39b0a59c90
Kim, Jaeung <kimjw5076@naver.com>
2025-12-11 17:37:41 +0900

    day10(1211) 프로그래머스 문제2

---
+++ 14-java/src/day10/Ex04.java
@@ -0,0 +1,37 @@
+package day10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Ex04 {
+    public int[] solution(int []arr) {
+        // 자연수를 담을 ArrayList라는 빈 리스트를 만들고
+        ArrayList<Integer> list = new ArrayList<>();
+
+        // 첫번째수는 절대 중복되지 않으니 저장
+        list.add(arr[0]);
+
+        // 이전 숫자와 다르면 추가
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[i - 1]) {
+                list.add(arr[i]);
+            }
+        }
+
+        // list에 저장한 값을 answer 배열에 복사
+        int[] answer = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+
+        return answer;
+    }
+
+    // 테스트용
+    public static void main(String[] args) {
+        Ex04 solution = new Ex04();
+        int[] array = new int[]{1, 1, 3, 3, 0, 1, 1, 5, 6, 5};
+
+        System.out.println(Arrays.toString(solution.solution(array)));
+    }
+}
